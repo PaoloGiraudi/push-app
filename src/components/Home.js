f006c0a929466996ed062a897f9aa7bb393f414b
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-01-24 16:14:59 +0100

    Home page completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 import { Container, Text } from "./Login";
+import { BsArrowUp, BsArrowDownLeft } from "react-icons/bs";
+
+const ArrowUp = styled(BsArrowUp)`
+  font-size: 3rem;
+  margin-bottom: 1.5rem;
+`;
+
+const ArrowDownLeft = styled(BsArrowDownLeft)`
+  font-size: 3rem;
+  margin-right: 2rem;
+  margin-top: 1rem;
+`;
+
+const TextBig = styled(Text)`
+  font-size: 1.25rem;
+  margin-bottom: 4rem;
+`;
 
 export default function Home({ children, ...props }) {
   return (
     <Container>
-      <Text>Welcome to the pushapp</Text>
+      <ArrowUp />
+      <TextBig>This is the number of pushups made by Paolo in 2021.</TextBig>
+
+      <Text>More info here</Text>
+      <ArrowDownLeft />
     </Container>
   );
 }
