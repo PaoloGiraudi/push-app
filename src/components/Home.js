fa48ccda4336eaddd8412aa764ac6c85ee4ee62a
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-03-25 21:04:00 +0100

    homepage cleanup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 import { Container, Text } from "./Login";
-import { BsArrowUp, BsArrowDownLeft } from "react-icons/bs";
-
-const ArrowUp = styled(BsArrowUp)`
-  font-size: 3rem;
-  margin-bottom: 1.5rem;
-`;
-
-const ArrowDownLeft = styled(BsArrowDownLeft)`
-  font-size: 2rem;
-  margin-right: 2rem;
-
-`;
 
 const TextBig = styled(Text)`
-  font-size: 1.25rem;
-  margin-bottom: 6rem;
+  font-size: 1.75rem;
+  margin-bottom: 2rem;
+  
 `;
 
 const TextSmall = styled(Text)`
-  font-size: 1rem;
-  margin-bottom: 1rem;
-
+  font-size: 1.25rem;
+  padding: 0 1rem;
 `;
 
 export default function Home({ children, ...props }) {
   return (
     <Container>
-      <ArrowUp />
-      <TextBig>This is the number of pushups by Paolo in 2021.</TextBig>
-      <TextSmall>Few more info here</TextSmall>
-      <ArrowDownLeft />
+      <TextBig>Welcome to Push App!</TextBig>
+      <TextSmall>
+        It is a simple database that keeps track of the push ups I have done during
+        2021.
+      </TextSmall>
     </Container>
   );
 }
